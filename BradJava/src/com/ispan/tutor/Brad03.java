0db9a25bc5dd3df7647494b617a0926325f4b075
mickeyyo55b <mickeyyo55b@gmail.com>
2025-09-22 13:59:08 +0800

    09221359

--- BradJava/src/com/ispan/tutor/Brad03.java
+++ BradJava/src/com/ispan/tutor/Brad03.java
@@ -6,11 +6,21 @@ public class Brad03 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("X = ");
 		int x = scanner.nextInt();
-		System.out.println(x);
+		//System.out.println(x);
+		System.out.print("Y = ");
 		int y = scanner.nextInt();
-		System.out.println(y);
-
+		//System.out.println(y);
+		int r1 = x + y;
+		int r2 = x - y;
+		int r3 = x * y;
+		int r4 = x / y;
+		int r5 = x % y;
+		System.out.printf("%d + %d = %d\n", x, y, r1);
+		System.out.printf("%d - %d = %d\n", x, y, r2);
+		System.out.printf("%d x %d = %d\n", x, y, r3);
+		System.out.printf("%d / %d = %d ...... %d\n", x, y, r4, r5);
 	}
 
 }
