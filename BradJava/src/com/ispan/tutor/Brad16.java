62aa859db38572fcfb42026717f8a3deca0e0d7a
mickeyyo55b <mickeyyo55b@gmail.com>
2025-09-23 12:04:07 +0800

    09231204

---
+++ BradJava/src/com/ispan/tutor/Brad16.java
@@ -0,0 +1,17 @@
+package com.ispan.tutor;
+
+public class Brad16 {
+	public static void main(String[] args) {
+		int[] a1 = new int[] {1,2,3,4,5};
+		System.out.println(a1.length);
+		System.out.println(a1[2]);
+		System.out.println("----");
+		int[] a2 = {11,22,33,44,55};
+		System.out.println(a2.length);
+		System.out.println(a2[2]);
+		System.out.println("----");
+		int[] a3;
+		a3 = new int[]{111,222,333,444,555};
+		
+	}
+}
---
+++ BradJava/src/com/ispan/tutor/Brad17.java
@@ -0,0 +1,20 @@
+package com.ispan.tutor;
+
+public class Brad17 {
+
+	public static void main(String[] args) {
+		int[][] ary;
+		ary = new int[2][3];
+		System.out.println(ary[0][1]);
+		System.out.println("----");
+		int[][] a = new int[3][];
+		a[0] = new int[2];
+		a[1] = new int[3];
+		a[2] = new int[4];
+		System.out.println(a);
+		System.out.println(a[0]);
+		System.out.println(a[2].length);
+
+	}
+
+}
