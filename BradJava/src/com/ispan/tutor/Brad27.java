d43221735ca84eea2b38d3132af022fcce0fc4d3
mickeyyo55b <mickeyyo55b@gmail.com>
2025-09-24 16:06:07 +0800

    09241606

--- BradJava/src/com/ispan/tutor/Brad27.java
+++ BradJava/src/com/ispan/tutor/Brad27.java
@@ -5,13 +5,8 @@ import com.ispan.apis.TWId;
 public class Brad27 {
 
 	public static void main(String[] args) {
-		double rand = Math.random();
-		System.out.println(rand);
-		boolean isRightID = TWId.isRight("B423456789");
-		System.out.println(isRightID);
-		
-		String id = "@223456789";
-		System.out.println(id.matches("[A-Z][12][0-9]{8}"));
+		String id = "A123456789";
+		System.out.println(TWId.isRight(id));
 		
 		
 	}
