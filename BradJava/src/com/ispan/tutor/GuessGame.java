98f6a528ba887a24acb8b5e53a27bf9cc771fd85
mickeyyo55b <mickeyyo55b@gmail.com>
2025-09-25 12:00:00 +0800

    09251159

--- BradJava/src/com/ispan/tutor/GuessGame.java
+++ BradJava/src/com/ispan/tutor/GuessGame.java
@@ -1,15 +1,43 @@
 package com.ispan.tutor;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class GuessGame extends JFrame{
+	private JTextField input;
+	private JButton guess;
+	private JTextArea log;
 	
 	public GuessGame() {
+		super("猜數字遊戲");
+		
+		input = new JTextField();
+		guess = new JButton("猜");
+		log = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		add(log, BorderLayout.CENTER);
+		
+		JPanel top = new JPanel(new BorderLayout());
+		top.add(guess, BorderLayout.EAST);
+		top.add(input, BorderLayout.CENTER);
+		
+		add(top, BorderLayout.NORTH);
+		
 		
 		setSize(640, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 		new GuessGame();
