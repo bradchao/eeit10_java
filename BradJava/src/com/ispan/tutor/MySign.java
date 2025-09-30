fe769ce1032d37f06a9948f7a4751490e8ec8827
mickeyyo55b <mickeyyo55b@gmail.com>
2025-09-30 15:44:52 +0800

    09301544

--- BradJava/src/com/ispan/tutor/MySign.java
+++ BradJava/src/com/ispan/tutor/MySign.java
@@ -1,13 +1,17 @@
 package com.ispan.tutor;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.ispan.apis.MyDrawer;
 
 public class MySign extends JFrame {
 	private MyDrawer myDrawer;
+	private JButton clear, undo, redo;
 	
 	public MySign() {
 		super("簽名 App");
@@ -15,6 +19,14 @@ public class MySign extends JFrame {
 		setLayout(new BorderLayout());
 		myDrawer = new MyDrawer();
 		add(myDrawer, BorderLayout.CENTER);
+		
+		JPanel top = new JPanel(new FlowLayout());
+		clear = new JButton("清除");
+		undo = new JButton("上一步");
+		redo = new JButton("復原");
+		top.add(clear); top.add(undo);top.add(redo);
+		
+		add(top, BorderLayout.NORTH);
 		
 		setSize(800, 480);
 		setVisible(true);
