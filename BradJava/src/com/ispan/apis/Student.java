3720a832ae8cf1d75c1d844f1ef74ad4d78e5c2f
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-02 11:07:26 +0800

    10021107

--- BradJava/src/com/ispan/apis/Student.java
+++ BradJava/src/com/ispan/apis/Student.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 public class Student implements Serializable{
 	private String name;
 	private int ch, eng, math;
+	private Bike bike;
+	
 	public Student(String name, int ch, int eng, int math) {
 		this.name = name;
 		this.ch = ch;
 		this.eng = eng;
 		this.math = math;
+		bike = new Bike();
 	}
 	public int score() {return ch+eng+math;}
 	public double avg() {return score()/3.0;}
