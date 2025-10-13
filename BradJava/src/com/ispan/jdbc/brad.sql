6211a2e1a3a3acc4e36061288a45aebec992fdc7
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-13 16:31:38 +0800

    10131631

--- BradJava/src/com/ispan/jdbc/brad.sql
+++ BradJava/src/com/ispan/jdbc/brad.sql
@@ -67,10 +67,31 @@ HAVING
 ORDER BY
 LIMIT 
 -----------------------------------------------------------
-
-
-
-
+SELECT `ProductID`, SUM(`Quantity`) sum
+FROM `orderdetails` 
+GROUP BY `ProductID`
+HAVING sum < 200
+-----------------------------------------------------------
+SELECT `CompanyName`,`ContactName`,`Phone` 
+FROM `customers` 
+WHERE `CustomerID` = (
+    SELECT CustomerID 
+    FROM orders
+    WHERE OrderID = 10254
+)
+-------------------------------------
+SELECT CompanyName FROM customers 
+WHERE CustomerID IN (
+    SELECT CustomerID FROM `orders`
+    WHERE OrderDate BETWEEN '1997-01-01' AND '1997-12-31'
+)
+-------------------------------
+SELECT `ProductName` FROM `products` 
+WHERE `CategoryID` = (
+    SELECT `CategoryID` 
+    FROM `categories` 
+    WHERE `CategoryName` = 'Seafood'
+)
 
 
 
