943aa03fa4a21b9e6b5871da62536b6b9b5049d1
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-13 10:34:40 +0800

    10131034

--- BradJava/src/com/ispan/jdbc/Brad06.java
+++ BradJava/src/com/ispan/jdbc/Brad06.java
@@ -7,31 +7,45 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Brad06 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Keyword:");
+		String key = scanner.next();
+		String search = "%" + key + "%";
+		
+		
 		String url = "jdbc:mysql://localhost:3306/iii";
 		Properties prop = new Properties();
 		prop.put("user", "root");
 		prop.put("password", "root");
 		
 		String sql = """
-				SELECT id, name, city country, town, addr
+				SELECT id, name, city, town, addr
 				FROM gift
+				WHERE name LIKE ? OR city LIKE ? OR town LIKE ? OR addr LIKE ?
+				ORDER BY city, town, addr
 				""";
 		try (Connection conn = DriverManager.getConnection(url, prop);) {
 			
 			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, search);
+			pstmt.setString(2, search);
+			pstmt.setString(3, search);
+			pstmt.setString(4, search);
+			
 			ResultSet rs = pstmt.executeQuery();
 			
 			while (rs.next()) {
 				String id = rs.getString("id");
 				String name = rs.getString("name");
-				String country = rs.getString("country");
+				String city = rs.getString("city");
 				String town = rs.getString("town");
 				String addr = rs.getString("addr");
 				System.out.printf("%s : %s : %s%s%s\n", 
-						id, name, country, town, addr);
+						id, name, city, town, addr);
 			}
 			
 			
--- BradJava/src/com/ispan/jdbc/Brad07.java
+++ BradJava/src/com/ispan/jdbc/Brad07.java
@@ -3,33 +3,51 @@ package com.ispan.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Brad07 {
+	private static final String url = "jdbc:mysql://localhost:3306/iii";
+	private static final String USER = "root";
+	private static final String PASSWD = "root";
+	private static final String SQL = """
+			SELECT id, name, city, town, addr
+			FROM gift
+			ORDER BY id
+			LIMIT ?, ?
+			""";
+	
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/iii";
-		Properties prop = new Properties();
-		prop.put("user", "root");
-		prop.put("password", "root");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Page:");		// 1~
+		int page = scanner.nextInt();
 		
-		String sqlInsert = """
-				INSERT INTO cust 
-					(cname, tel, birthday) 
-				VALUES 
-					(?,?,?)
-				""";
+		final int rpp = 7;	// rows per page
+		int start = (page - 1) * rpp;
+		
+		Properties prop = new Properties();
+		prop.put("user", USER);
+		prop.put("password", PASSWD);
+		
 		try (Connection conn = DriverManager.getConnection(url, prop);) {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, rpp);			
 			
-			PreparedStatement pstmt = conn.prepareStatement(sqlInsert);
+			ResultSet rs = pstmt.executeQuery();
 			
-			pstmt.setString(1, "Mark");
-			pstmt.setString(2, "222");
-			pstmt.setString(3, "1999-10-10");
-			int n = pstmt.executeUpdate();
-			System.out.println("OK:" + n);
-			
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String name = rs.getString("name");
+				String city = rs.getString("city");
+				String town = rs.getString("town");
+				String addr = rs.getString("addr");
+				System.out.printf("%s : %s : %s%s%s\n", 
+						id, name, city, town, addr);
+			}			
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
