399f8e5b5555dad4a6cb5e28595599e712955fe3
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-13 15:05:36 +0800

    10131505

--- BradJava/src/com/ispan/jdbc/brad.sql
+++ BradJava/src/com/ispan/jdbc/brad.sql
@@ -1,4 +1,41 @@
 SELECT Title, FirstName, LastName 
 FROM `employees`
 ORDER BY Title ASC, FirstName DESC
++--------------------------+-----------+-----------+
+| Title                    | FirstName | LastName  |
++--------------------------+-----------+-----------+
+| Inside Sales Coordinator | Laura     | Callahan  |
+| Sales Manager            | Steven    | Buchanan  |
+| Sales Representative     | Robert    | King      |
+| Sales Representative     | Nancy     | Davolio   |
+| Sales Representative     | Michael   | Suyama    |
+| Sales Representative     | Margaret  | Peacock   |
+| Sales Representative     | Janet     | Leverling |
+| Sales Representative     | Anne      | Dodsworth |
+| Vice President, Sales    | Andrew    | Fuller    |
++--------------------------+-----------+-----------+
+
+Title   	FirstName   	LastName	
+Inside Sales Coordinator	Laura	Callahan	
+Sales Manager	Steven	Buchanan	
+Sales Representative	Robert	King	
+Sales Representative	Nancy	Davolio	
+Sales Representative	Michael	Suyama	
+Sales Representative	Margaret	Peacock	
+Sales Representative	Janet	Leverling	
+Sales Representative	Anne	Dodsworth	
+Vice President, Sales	Andrew	Fuller	
+-----------------------------------------------------------
+SELECT Title, FirstName, LastName 
+FROM `employees`
+WHERE Title <> 'Sales Representative'
+ORDER BY Title ASC, FirstName DESC;
+
+
+Title   	FirstName   	LastName	
+Inside Sales Coordinator	Laura	Callahan	
+Sales Manager	Steven	Buchanan	
+Vice President, Sales	Andrew	Fuller	
+-----------------------------------------------------------
+
 
