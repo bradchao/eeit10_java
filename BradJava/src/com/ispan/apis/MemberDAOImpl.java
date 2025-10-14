cf68f5f66354eec7a611d0b8d9fc296d86d42618
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-14 10:14:16 +0800

    10141014

--- BradJava/src/com/ispan/apis/MemberDAOImpl.java
+++ BradJava/src/com/ispan/apis/MemberDAOImpl.java
@@ -2,6 +2,8 @@ package com.ispan.apis;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MemberDAOImpl implements MemberDAO{
@@ -17,6 +19,9 @@ public class MemberDAOImpl implements MemberDAO{
 			""";
 	private final String SQL_FIND_ID = """
 			SELECT id, account, passwd, name FROM member WHERE id = ?
+			""";
+	private final String SQL_FIND_ALL = """
+			SELECT id, account, passwd, name FROM member
 			""";
 	
 	public MemberDAOImpl (Connection conn) {
@@ -56,15 +61,37 @@ public class MemberDAOImpl implements MemberDAO{
 	}
 
 	@Override
-	public Member findById(int id) {
-		// TODO Auto-generated method stub
+	public Member findById(int id) throws Exception {
+		try(PreparedStatement pstmt = conn.prepareStatement(SQL_FIND_ID)) {
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getInt("id"));
+				member.setAccount(rs.getString("account"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setName(rs.getString("name"));
+				return member;
+			}
+		}		
 		return null;
 	}
 
 	@Override
-	public List<Member> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Member> findAll() throws Exception {
+		List<Member> members = new ArrayList<Member>();
+		try(PreparedStatement pstmt = conn.prepareStatement(SQL_FIND_ALL)) {
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getInt("id"));
+				member.setAccount(rs.getString("account"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setName(rs.getString("name"));
+				members.add(member);
+			}
+		}		
+		return members;
 	}
 
 }
