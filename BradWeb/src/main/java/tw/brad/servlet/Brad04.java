90994b655baf8b68d96ea386f2979f583838792b
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-15 13:15:24 +0800

    101501

--- BradWeb/src/main/java/tw/brad/servlet/Brad04.java
+++ BradWeb/src/main/java/tw/brad/servlet/Brad04.java
@@ -26,7 +26,7 @@ public class Brad04 extends HttpServlet {
 			//--------------------------------
 			response.setContentType("text/html; charset=UTF-8");
 			PrintWriter out = response.getWriter();
-			out.printf("%s + %s = %d", x, y, (intX + intY));
+			out.printf("%s +  %s = %d", x, y, (intX + intY));
 		}catch(Exception e) {
 			System.out.println(e);
 		}
