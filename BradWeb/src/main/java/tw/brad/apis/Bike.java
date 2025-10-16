d6062456726048133c6d5bd1a0c3c9e04b7a6c63
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-16 12:00:55 +0800

    10161200

---
+++ BradWeb/src/main/java/tw/brad/apis/Bike.java
@@ -0,0 +1,26 @@
+package tw.brad.apis;
+
+import java.io.Serializable;
+
+public class Bike extends Object implements Serializable{
+	protected double speed;
+	
+	public Bike upSpeed() {
+		speed = speed < 1 ? 1 : speed * 1.4;
+		return this;
+	}
+	public Bike downSpeed() {
+		speed = speed < 1 ? 0 : speed * 0.7;
+		return this;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Speed = %f", speed);
+	}
+	
+}
---
+++ BradWeb/src/main/java/tw/brad/servlet/Brad12.java
@@ -0,0 +1,26 @@
+package tw.brad.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tw.brad.apis.Bike;
+
+@WebServlet("/brad12")
+public class Brad12 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		Integer x =  (Integer)request.getAttribute("x");
+		Integer y =  (Integer)request.getAttribute("y");
+		Bike bike = (Bike)request.getAttribute("bike");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<div>I am Brad12<div>");
+		out.printf("%d + %d = %d<br />", x, y, (x+y));
+		out.print(bike);
+	}
+}
