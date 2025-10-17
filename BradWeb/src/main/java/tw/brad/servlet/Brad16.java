8aea142ff4432640e787e51f4a3bbf15389ba856
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-17 09:40:35 +0800

    10170940

--- BradWeb/src/main/java/tw/brad/servlet/Brad16.java
+++ BradWeb/src/main/java/tw/brad/servlet/Brad16.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tw.brad.apis.BradUtils;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -15,8 +17,16 @@ import java.io.PrintWriter;
 public class Brad16 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		String webContent;
+		try {
+			webContent = BradUtils.loadView();
+		} catch (Exception e) {
+			webContent = "ERROR Page";
+		}
+		
+		
 		PrintWriter out = response.getWriter();
-		out.print("web content");
+		out.print(webContent);
 		response.flushBuffer();
 	}
 
