90654113df7b17c403fa81bfdd49e14fdbeae112
mickeyyo55b <mickeyyo55b@gmail.com>
2025-10-20 13:43:48 +0800

    10201543

---
+++ BradWeb/src/main/java/tw/brad/websocket/MyCenter.java
@@ -0,0 +1,56 @@
+package tw.brad.websocket;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+//@ServerEndpoint("/mycenter")
+public class MyCenter {
+	private static HashSet<Session> sessions;
+	
+	
+	public MyCenter() {
+		System.out.println("MyServer()");
+		if (sessions == null) {
+			sessions = new HashSet<>();
+		}
+	}
+
+	@OnOpen
+	public void onOpen(Session session) {
+		System.out.println("onOpen");
+		if (sessions.add(session)) {
+			System.out.println("new session");
+		}
+	}
+	
+	@OnMessage
+	public void onMessage(String mesg, Session session) {
+		//System.out.println("onMessage:" + mesg);
+		for (Session userSession: sessions) {
+			try {
+				userSession.getBasicRemote().sendText(mesg);
+			} catch (IOException e) {
+				
+			}
+		}
+	}
+	
+	@OnClose
+	public void onClose(Session session) {
+		System.out.println("onClose");;
+		sessions.remove(session);
+	}
+	
+	@OnError
+	public void onError(Session session, Throwable t) {
+		System.out.println("onError:" + t.toString());;
+	}
+	
+}
